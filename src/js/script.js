f4abf8e3d37917f39d7926bc5912deb6f145e0a8
Wojciech Bielecki <wojbiel833@gmail.com>
2021-02-12 15:31:26 +0100

    Add renderInMenu & initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  'use strict';
+  ('use strict');
 
   const select = {
     templateOf: {
@@ -45,21 +45,99 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
+      // console.log(`newProduct:`, thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generateHTML = templates.menuProduct(thisProduct.data);
+      // console.log(generateHTML);
+      /* create element usint utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generateHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      // console.log(menuContainer);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = document.querySelector(
+        select.menuProduct.clickable
+      );
+      console.log(clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        console.log('click');
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = clickableTrigger.querySelector('active');
+        console.log(activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct && !thisProduct) {
+          clickableTrigger.classList.remove('active');
+        }
+        /* toggle active class on thisProduct.element */
+        clickableTrigger.classList.toggle('active');
+      });
+    }
+  }
   const app = {
-    init: function(){
+    initMenu: function () {
+      const thisApp = this;
+
+      console.log(`thisApp.data:`, thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      // DLA POJEDYŃCZEGO ELEMENTU
+      // const testProduct = new Product();
+      // console.log(`testProduct:`, testProduct);
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
