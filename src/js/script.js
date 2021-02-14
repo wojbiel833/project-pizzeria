9de26084431338390718051f625301f622e3f007
Wojciech Bielecki <wojbiel833@gmail.com>
2021-02-14 11:52:06 +0100

    Add images display

--- src/js/script.js
+++ src/js/script.js
@@ -111,6 +111,11 @@
         select.menuProduct.priceElem
       ); // .product__total-price .price
       console.log(thisProduct.priceElem);
+
+      thisProduct.imageWrapper = thisProduct.element.querySelector(
+        select.menuProduct.imageWrapper
+      ); //.product__images
+      console.log(thisProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -172,9 +177,23 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+          // access to[param]-[option]' with images
+          const optionImage = thisProduct.imageWrapper.querySelectorAll(
+            `.${paramId}-${optionId}`
+          );
+          console.log(optionImage);
+          const activeImg = document.querySelector(
+            `.product__images .${paramId}-${optionId}`
+          );
+          console.log(activeImg);
+          if (optionImage) {
+            // activeImg.classList.remove('active');
+            // console.log(activeImg);
+          }
 
-          // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
+            if (option) thisProduct.imageWrapper.innerHTML += activeImg;
+
             // check if the option is not default
             if (!option.default) {
               // add option price to price variable
@@ -215,11 +234,11 @@
 
     init: function () {
       const thisApp = this;
-      // console.log('*** App starting ***');
-      // console.log('thisApp:', thisApp);
-      // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
-      // console.log('templates:', templates);
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
